3c5b7db486cfa01c1a2a3afa89c03b93c637e390
xiaohuihui <948250436@qq.com>
2015-06-11 13:05:38 +0800

    add SQLite helper class,and model of table

---
+++ src/main/java/com/guohuili/administrator/huiweather/model/City.java
@@ -0,0 +1,44 @@
+package com.guohuili.administrator.huiweather.model;
+
+/**
+ * Created by Administrator on 2015/6/11 0011.
+ */
+public class City {
+
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ src/main/java/com/guohuili/administrator/huiweather/model/County.java
@@ -0,0 +1,44 @@
+package com.guohuili.administrator.huiweather.model;
+
+/**
+ * Created by Administrator on 2015/6/11 0011.
+ */
+public class County {
+
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
